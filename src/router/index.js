ab8c65a53bc338b0b7f71609875cd8e1928134e1
AN-248 <cctyranitar21@gmail.com>
2022-09-01 22:33:55 +0900

    add pages

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,16 @@ const routes= [
     path: '/',
     name: 'home',
     component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/new',
+    name: 'new',
+    component: () => import('../views/New.vue')
+  },
+  {
+    path: '/felt',
+    name: 'felt',
+    component: () => import('../views/Felt.vue')
   }
 ]
 
